20fec5ade268dbc2e96fed2e230060fad1033e22
DCoder-1 <lakshayparasharkumar721@gmail.com>
2022-10-30 23:40:08 +0530

    implemented the date validation with regex pattern check & basic info

--- controllers/coin.js
+++ controllers/coin.js
@@ -1,4 +1,3 @@
-const moment = require('moment')
 const Axios = require('axios')
 
 class Controller {
@@ -6,10 +5,40 @@ class Controller {
     try {
       const { id, date } = req.query
       if (!date) return res.status(200).json({ status: 0, message: 'Please provide a date' })
-      if (!moment(date, 'DD-MM-YYYY').isValid()) return res.status(200).json({ status: 0, message: 'Date you entered is invalid.' })
-      const history = await Axios.get(`https://api.coingecko.com/api/v3/coins/${id}/history?date=${date}&localization=false`)
+      console.log(typeof date)
+      const [dateValue, month, year] = date.split('-')
+      const dateCheck = /^(0?[1-9]|[12][0-9]|3[01])$/
+      const monthCheck = /^(0[1-9]|1[0-2])$/
+      const yearCheck = /^\d{4}$/
 
-      res.status(200).json({ status: 1, message: 'OK', data: history.data })
+      if (month.match(monthCheck) && dateValue.match(dateCheck) && year.match(yearCheck)) {
+        const ListofDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+        if (month !== 2) {
+          if (dateValue > ListofDays[month - 1]) {
+            return res.status(200).json({ status: 0, message: 'Date you entered is invalid.' })
+          }
+        }
+        if (month === 2) {
+          const leapYear = false
+          if ((!(year % 4) && year % 100) || !(year % 400)) {
+            leapYear = true
+          }
+
+          if (leapYear === false && dateValue >= 29) {
+            return res.status(200).json({ status: 0, message: 'Date you entered is invalid.' })
+          }
+
+          if (leapYear === true && dateValue > 29) {
+            return res.status(200).json({ status: 0, message: 'Date you entered is invalid.' })
+          }
+        }
+        const history = await Axios.get(`https://api.coingecko.com/api/v3/coins/${id}/history?date=${date}&localization=false`)
+
+        res.status(200).json({ status: 1, message: 'OK', data: history.data })
+      } else {
+        return res.status(200).json({ status: 0, message: 'Date you entered is invalid.' })
+      }
     } catch (err) {
       next(err)
     }
